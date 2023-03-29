8d04cb07a643a59208e940463b2d03bdbda595a1
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-03-29 17:04:50 +0900

    add: vue init
    init, sort

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<!--
+An example of creating a reusable grid component and using it with external data.
+-->
+
+<script setup>
+import DemoGrid from './components/Grid.vue'
+import { ref } from 'vue'
+
+const searchQuery = ref('')
+const gridColumns = ['name', 'power']
+const gridData = [
+  { name: 'bysuco', power: Infinity },
+  { name: 'break', power: 9000 },
+  { name: 'cread', power: 7000 },
+  { name: 'jomalon', power: 8000 }
+]
+</script>
+
+<template>
+  <form id="search">
+    Search <input name="query" v-model="searchQuery">
+  </form>
+  <DemoGrid :data="gridData" :columns="gridColumns" :filter-key="searchQuery">
+  </DemoGrid>
+</template>
