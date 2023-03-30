b725dd152db4243ce91937aeb50ab5225951baa9
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-03-30 10:01:01 +0900

    add: TreeData

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,49 @@
 <!--
-An example of creating a reusable grid component and using it with external data.
+A nested tree component that recursively renders itself.
+You can double click on an item to turn it into a folder.
 -->
 
 <script setup>
-import DemoGrid from './components/Grid.vue'
 import { ref } from 'vue'
+import TreeItem from './components/TreeItem.vue'
 
-const searchQuery = ref('')
-const gridColumns = ['name', 'power']
-const gridData = [
-  { name: 'bysuco', power: Infinity },
-  { name: 'break', power: 9000 },
-  { name: 'cread', power: 7000 },
-  { name: 'jomalon', power: 8000 }
-]
+const treeData = ref({
+  name: 'Bysuco',
+  children: [
+    { name: 'menu' },
+    { name: 'item' },
+    {
+      name: 'child folder',
+      children: [
+        {
+          name: 'child folder',
+          children: [{ name: 'hello' }, { name: 'world' }]
+        },
+        { name: 'hello' },
+        { name: 'world' },
+        {
+          name: 'child folder',
+          children: [{ name: 'hello' }, { name: 'world' }]
+        }
+      ]
+    }
+  ]
+})
 </script>
 
 <template>
-  <form id="search">
-    Search <input name="query" v-model="searchQuery">
-  </form>
-  <DemoGrid :data="gridData" :columns="gridColumns" :filter-key="searchQuery">
-  </DemoGrid>
+  <ul>
+    <TreeItem class="item" :model="treeData"></TreeItem>
+  </ul>
 </template>
+
+<style>
+.item {
+  cursor: pointer;
+  line-height: 1.5;
+}
+
+.bold {
+  font-weight: bold;
+}
+</style>
